f0dab80084f33cb6c259682d6979b487a5ed24df
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2015-09-03 15:54:30 -0300

    Fix: adjust in rspec tests to NewsfeedController

--- spec/controllers/newsfeed_controller_spec.rb
+++ spec/controllers/newsfeed_controller_spec.rb
@@ -44,14 +44,15 @@ describe NewsfeedController do
       end
     end
 
-    context 'guest_user logged in and NOT invited' do
+
+    context 'guest_user logged in' do
       before(:each) do
         @user = User.new(guest: true, email: "guest_#{Time.now.to_i}#{rand(100)}@example.com")
         allow(controller).to receive(:current_user).and_return(@user)
         allow(@user).to receive(:city_slug).and_return(:albany)
-        allow(@user).to receive(:invited).and_return(false)
+        allow(@user).to receive(:invited).and_return(true)
       end
-      it 'should be block access for user' do
+      it 'should load newsfeed' do
         get :index, city: @user.city_slug, locale: :en
 
         expect(response).to redirect_to(newsfeed_city_path(:albany))
