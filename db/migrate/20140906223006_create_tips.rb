2bcdaa00be344a77c65cc262abbf2a1ad6e286a2
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2015-09-25 14:05:17 -0300

    Fix on migration errors with HABTM association

--- db/migrate/20140906223006_create_tips.rb
+++ db/migrate/20140906223006_create_tips.rb
@@ -2,7 +2,7 @@ class CreateTips < ActiveRecord::Migration
   def change
     create_table :tips do |t|
       t.text :description
-      t.references :events, index: true
+      t.references :event, index: true
 
       t.timestamps
     end
--- db/migrate/20150621225431_create_events_and_persona.rb
+++ db/migrate/20150621225431_create_events_and_persona.rb
@@ -1,7 +1,7 @@
 class CreateEventsAndPersona < ActiveRecord::Migration
   def change
     create_table :events_personas, id: false do |t|
-      t.belongs_to :events, index: true
+      t.belongs_to :event, index: true
       t.belongs_to :persona, index: true
     end
   end
