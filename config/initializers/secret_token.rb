93939db3fd1a28c48a72efc629b4fb6c310d3fdf
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2015-10-23 15:58:19 -0200

    Configured secret key base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,9 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-CidadeVc::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+CidadeVc::Application.config.secret_key_base = if Rails.env.development? or Rails.env.test?
+                                                 ('x' * 30)
+                                               else
+                                                 ENV['SECRET_KEY_BASE']
+                                               end
+
