63d433680907d2213cd54e710b50f893e274d80c
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2015-10-25 17:01:10 -0200

    Refactor in JSONModule

--- app/domain/json/item/item_to_json.rb
+++ app/domain/json/item/item_to_json.rb
@@ -6,8 +6,8 @@ module Villeme
         @item = item
         @options = options
         @distance = calculate_distance_from_user_to_item
-        @action = define_action_name_to_url(@item)
-        @user = @options[:user]
+        @action = define_action_name_to_url(item)
+        @user = options[:user]
 
         create_json
       end
