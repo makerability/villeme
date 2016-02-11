11694e08fd2692fc37010d8911fe6c00d4408af8
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2016-02-11 16:55:25 -0200

    Fix in test to create_activity

--- spec/features/activities/create_activity_spec.rb
+++ spec/features/activities/create_activity_spec.rb
@@ -32,28 +32,28 @@ feature 'Create an activity', js: true do
     scenario 'should create an activity' do
       visit('/en/activities/new')
 
-        sleep 3
+      sleep 3
 
-        page.fill_in 'activity[name]', with: @activity_attributes.name
-        page.evaluate_script("$('.jqte_editor').text('#{@activity_attributes.description}')")
-        page.fill_in 'activity[date_start]', with: Date.current.strftime('%d/%m/%Y')
-        page.fill_in 'activity[date_finish]', with: (Date.current + 10).strftime('%d/%m/%Y')
-        # page.check 'Monday'
-        # page.check 'Sunday'
+      page.fill_in 'activity[name]', with: @activity_attributes.name
+      page.evaluate_script("$('.jqte_editor').text('#{@activity_attributes.description}')")
+      page.fill_in 'activity[date_start]', with: Date.current.strftime('%d/%m/%Y')
+      page.fill_in 'activity[date_finish]', with: (Date.current + 10).strftime('%d/%m/%Y')
+      # page.check 'Monday'
+      # page.check 'Sunday'
 
-        sleep 3
+      sleep 3
 
-        page.fill_in 'activity[hour_start_first]', with: '09:00'
-        page.fill_in 'activity[hour_finish_first]', with: '20:30'
-        attach_file 'activity[image]', "#{Rails.root}/public/images/default-event-image.jpg"
-        # fill_in '#event_cost_fake', with: '10,00'
-        page.fill_in 'activity[place_attributes][name]', with: 'Parque da Redenção'
-          find_field('activity[place_attributes][name]').native.send_keys(:return)
+      page.fill_in 'activity[hour_start_first]', with: '09:00'
+      page.fill_in 'activity[hour_finish_first]', with: '20:30'
+      attach_file 'activity[image]', "#{Rails.root}/public/images/default-event-image.jpg"
+      # fill_in '#event_cost_fake', with: '10,00'
+      page.fill_in 'activity[place_attributes][name]', with: 'Parque da Redenção'
+        find_field('activity[place_attributes][name]').native.send_keys(:return)
 
-        sleep 3
+      sleep 3
 
-        page.fill_in 'activity[address]', with: @activity_attributes.address
-
+      page.execute_script("$('.criar-lugar').show()")
+      page.fill_in 'activity[address]', with: @activity_attributes.address
 
       page.click_button 'submit'
 
